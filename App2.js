89e29c723b0499e2f852eefde165c453156da90b
Maicon Luis Silva <maiconlsilva100@gmail.com>
2023-11-03 12:50:02 -0300

    alteração no numero aleatorio

--- App2.js
+++ App2.js
@@ -6,7 +6,7 @@
 
 let numeroAleatorio = geraNumeroAleatorio();
 let tentativa = 1
-let limiteNumeroAleatorio = 100
+let MaximoNumeroAleatorio = 100
 
 function exibirTextoNaTela(tag, texto){
 let campo = document.querySelector(tag);
